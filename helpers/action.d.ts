a2ce737846641bd1f92142cad4c4fb75204d7efb
turturean <dorinturturean@yahoo.com>
2021-11-15 22:07:26 -0700

    init

--- helpers/action.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function createActionTypes(actionTypes: any): <T>(type: any) => any;
-export declare function createAction(type: any, action: any): (payload: any) => {
-    type: any;
-    payload: any;
-};
--- lib/flow-adapters.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class FlowAdaptersService {
-    constructor();
-}
